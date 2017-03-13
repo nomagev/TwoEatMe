e4faa462d7c5596ba31623f286046452e6573a60
Nomagev <vegamontesino@msn.com>
2017-03-13 23:26:06 +0100

    starting to work with tweepy code: still unsuccessful

--- code/hellotweepy.py
+++ code/hellotweepy.py
@@ -98,18 +98,18 @@ if fileexists is False: # If Keys.txt does not exists, create it and store value
     print "Let's go then"
     print ""
     credentials = open("keys.txt", "w")
-    consumerkey = raw_input("Enter your Consumer Key: ")
-    consumerkey = str(consumerkey) + '\n'
-    credentials.write(consumerkey)
-    consumersecret = raw_input("Enter your Consumer Secret Key: ")
-    consumersecret = str(consumersecret) + '\n'
-    credentials.write(consumersecret)
-    accesstoken = raw_input("Enter your Access Token: ")
-    accesstoken = str(accesstoken) + '\n'
-    credentials.write(accesstoken)
-    accesstokensecret = raw_input("Enter your Access Token Secret Key: ")
-    accesstokensecret = str(accesstokensecret) + '\n'
-    credentials.write(accesstokensecret)
+    consumer_key = raw_input("Enter your Consumer Key: ")
+    consumer_key = str(consumer_key) + '\n'
+    credentials.write(consumer_key)
+    consumer_secret = raw_input("Enter your Consumer Secret Key: ")
+    consumer_secret = str(consumer_secret) + '\n'
+    credentials.write(consumer_secret)
+    access_token = raw_input("Enter your Access Token: ")
+    access_token = str(access_token) + '\n'
+    credentials.write(access_token)
+    access_token_secret = raw_input("Enter your Access Token Secret Key: ")
+    access_token_secret = str(access_token_secret) + '\n'
+    credentials.write(access_token_secret)
     credentials.close()
 
 else:
@@ -117,28 +117,28 @@ else:
     credentials = open("keys.txt", "r") # Open keys.txt in Read Mode and
     for line in credentials: # run a loop into the content of the file
         keylist.append(line) # add each value stored @ keys.txt into the keylist
-    consumerkey = str(keylist[0]) # assign 1st value in keylist to this variable
-    consumersecret = str(keylist[1]) # assign 2nd value in keylist to this variable
-    accesstoken = str(keylist[2]) # assign 3rd value in keylist to this variable
-    accesstokensecret = str(keylist[3]) # assign 4th value in keylist to this variable
+    consumer_key = str(keylist[0]) # assign 1st value in keylist to this variable
+    consumer_secret = str(keylist[1]) # assign 2nd value in keylist to this variable
+    access_token = str(keylist[2]) # assign 3rd value in keylist to this variable
+    access_token_secret = str(keylist[3]) # assign 4th value in keylist to this variable
     credentials.close() # Close the file (it's no longer needed)
 
 # For testing purposes, I display the content of the variables
 
 print "For testing purposes, I display the content of the variables"
 print ""
-print "The Stored Consumer Key is ", consumerkey
-print "The Stored Consumer Secret is ", consumersecret
-print "The Stored Access Token is ", accesstoken
-print "The Stored Access Token Secret is ", accesstokensecret
+print "The Stored Consumer Key is", consumer_key
+print "The Stored Consumer Secret is", consumer_secret
+print "The Stored Access Token is", access_token
+print "The Stored Access Token Secret is", access_token_secret
 
 # Let's then request the different Twitter API keys required to run the code
 
-#authentication = tweepy.OAuthHandler(consumerkey, consumersecret)
-#authentication.set_access_token(accesstoken, accesstokensecret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-#twitteraccess = tweepy.API(authentication)
+api = tweepy.API(auth)
 
-#public_tweets = twitteraccess.home_timeline()
-#for tweet in public_tweets:
-#    print tweet.text
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
