67c6dd9e99468b8a4cba5d2ebb925b2498ca50fe
Nomagev <vegamontesino@msn.com>
2017-03-16 22:03:36 +0100

    Some Format correction to debug

--- code/testing-credentials.py
+++ code/testing-credentials.py
@@ -1,49 +1,52 @@
-import os, sys, tweepy
+import os
+import tweepy
+
+# pylint: disable=C0103
 
 fileexists = os.path.exists("keys-DO-NOT-COMMIT.txt")
 
 if fileexists is False:
     credentials = open("keys-DO-NOT-COMMIT.txt", "w")
-    consumer_key = raw_input("Enter your Consumer Key: ")
-    consumer_key = (consumer_key) + '\n'
-    credentials.write(consumer_key),
-    consumer_secret = raw_input("Enter your Consumer Secret Key: ")
-    consumer_secret = (consumer_secret) + '\n' 
-    credentials.write(consumer_secret)
-    access_token = raw_input("Enter your Access Token: ")
-    access_token = (access_token) + '\n'
-    credentials.write(access_token)
-    access_token_secret = raw_input("Enter your Access Token Secret Key: ")
-    access_token_secret = (access_token_secret)
-    credentials.write(access_token_secret)
+    consumerkey = raw_input("Enter your Consumer Key: ")
+    consumerkey = (consumerkey) + '\n'
+    credentials.write(consumerkey)
+    consumersecret = raw_input("Enter your Consumer Secret Key: ")
+    consumersecret = (consumersecret) + '\n'
+    credentials.write(consumersecret)
+    accesstoken = raw_input("Enter your Access Token: ")
+    accesstoken = (accesstoken) + '\n'
+    credentials.write(accesstoken)
+    accesstokensecret = raw_input("Enter your Access Token Secret Key: ")
+    accesstokensecret = (accesstokensecret)
+    credentials.write(accesstokensecret)
     credentials.close()
 
 else:
     keylist = []
     credentials = open("keys-DO-NOT-COMMIT.txt", "r")
     for line in credentials:
-        line.split
+        line.split()
         keylist.append(line)
-    consumer_key = (keylist[0])
-    consumer_secret = (keylist[1])
-    access_token = (keylist[2])
-    access_token_secret = (keylist[3])
+    consumerkey = (keylist[0])
+    consumersecret = (keylist[1])
+    accesstoken = (keylist[2])
+    accesstokensecret = (keylist[3])
     credentials.close()
 
-print "Consumer Key =", consumer_key
-print "Consumer Secret =", consumer_secret
-print "access_token =", access_token
-print "access_token_secret =", access_token_secret
+print "Consumer Key =", consumerkey
+print "Consumer Secret =", consumersecret
+print "Access Token =", accesstoken
+print "access Token Secret =", accesstokensecret
 
 print "Hello World"
 print "Hello World"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumerkey, consumersecret)
+auth.set_access_token(accesstoken, accesstokensecret)
 
 api = tweepy.API(auth)
 
-print(api.me().name)
+print api.me().name
 
 # keylist = []
 # credentials = open("keys-DO-NOT-COMMIT.txt", "r")
