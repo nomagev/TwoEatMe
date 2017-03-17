fae891d812b83cec22e2164a5206fb91de373d5c
Nomagev <vegamontesino@msn.com>
2017-03-17 21:04:24 +0100

    Changes on Credentials Step's Behavior

--- code/hellotweepy.py
+++ code/hellotweepy.py
@@ -8,12 +8,12 @@ import pickle
 try:
     import tweepy
 except ImportError:
-    print "--------------------------------------------"
-    print " Tweepy library is missing on your system!  "
-    print " Install it by using the following command: "
-    print "             pip install tweepy             "
-    print "Note:Use Admin Rights may be needed to do so"
-    print "--------------------------------------------"
+    print "-------------------------------------------------"
+    print "|    Tweepy method is missing on your system!    |"
+    print "|   Install it by using the following command:   |"
+    print "|               pip install tweepy               |"
+    print "|  Note:Use Admin Rights may be needed to do so  |"
+    print "-------------------------------------------------"
     os.abort()
 
 if sys.platform == "linux" or sys.platform == "linux2":
@@ -28,37 +28,37 @@ elif sys.platform == "win32":
 
 clear()
 
-print "---------------------------------------------"
-print "|  You need to use your own API credentials |"
-print "|   Please visit https://apps.twitter.com   |"
-print "| to create an App & gain required details: |"
-print "|                                           |"
-print "|           - Twitter Consumer Key          |"
-print "|       - Twitter Consumer Secret Key       |"
-print "|          - Twitter Access Token           |"
-print "|        - Twitter Access Token Secret      |"
-print "|                                           |"
-print "|    Once you have all of those details,    |"
-print "|      Please press a key to continue:      |"
-print "---------------------------------------------"
-
-pause()
-clear()
-
-filename =  os.path.basename(__file__)
+filename = os.path.basename(__file__)
 
 fileexists = os.path.exists("keys-DO-NOT-COMMIT.txt")
 
 if fileexists is False:
     print "-------------------------------------------------"
-    print "|     It seems you have no keys stored...       |"
-    print "|We are going to write & store your Twitter Keys|"
-    print "| so you do no have to worry about it anymore.  |"
+    print "|                      Hi!                      |"
+    print "|     It seems this is your first time here!    |"
+    print "|To use this program, there is MUST requirement!|"
+    print "|    Note: This will be a one-off exercise!     |"
+    print "|If everything works, you will not see me again |"
+    print "-------------------------------------------------"
+    pause()
+    clear()
+    print "-------------------------------------------------"
+    print "|  You need to use your own API credentials     |"
+    print "|   Please visit https://apps.twitter.com       |"
+    print "| to create an App & get the required details:  |"
+    print "|                                               |"
+    print "|           - Twitter Consumer Key              |"
+    print "|       - Twitter Consumer Secret Key           |"
+    print "|          - Twitter Access Token               |"
+    print "|        - Twitter Access Token Secret          |"
+    print "|                                               |"
+    print "|    Once you have all of those details,        |"
+    print "|      Please press a key to continue:          |"
     print "-------------------------------------------------"
     print ""
     print "-------------------------------------------------"
     print "|      We will store those keys into a file.    |"
-    print "|    This file will be called 'keys.txt' and    |"
+    print "|   This file is 'keys-DO-NOT-COMMIT.txt' and   |"
     print "|  it will be located into the directory where  |"
     print "|           you copied this program...          |"
     print "-------------------------------------------------"
@@ -66,18 +66,18 @@ if fileexists is False:
     print "-------------------------------------------------"
     print "|                  PLEASE NOTE:                 |"
     print "|      If you share this program, remember:     |"
-    print "|       DO NOT share keys.txt with anyone!      |"
-    print "| You only need to share the", filename, "file|"
+    print "|DON'T share keys-DO-NOT-COMMIT.txt with anyone!|"
+    print "|      Just share  the", filename, "file      |"
     print "-------------------------------------------------"
     print ""
     pause()
     print ""
-    print "Let's go then"
+    print "Okay! Let's do it then!"
     print ""
-    consumer_key = raw_input("Enter your Consumer Key: ")
-    consumer_secret = raw_input("Enter your Consumer Secret Key: ")
-    access_token = raw_input("Enter your Access Token: ")
-    access_token_secret = raw_input("Enter your Access Token Secret Key: ")
+    consumer_key = raw_input("1 - Enter your Consumer Key: ")
+    consumer_secret = raw_input("2 - Enter your Consumer Secret Key: ")
+    access_token = raw_input("3 - Enter your Access Token: ")
+    access_token_secret = raw_input("4 - Enter your Access Token Secret Key: ")
     keylist = [consumer_key, consumer_secret, access_token, access_token_secret]
     with open('keys-DO-NOT-COMMIT.txt', "wb") as keyloader:
         pickle.dump(keylist, keyloader)
