29e22f0da54a19b608137daa4acd6dee9a6437d6
Nomagev <vegamontesino@msn.com>
2017-03-18 23:45:57 +0100

    Exploring api.me().status arguments

--- code/testing-api.py
+++ code/testing-api.py
@@ -48,82 +48,121 @@ api = tweepy.API(auth)
 # This ones work for yourself, but they give a
 # lot of info, so it's better to keep them off
 # until we find the # way to work with them.
-# print(api.me())
-# print(api.me().status)
-
+# print api.me())
 
 # These ones retrieve specific data-points from
 # the selected user
 
-print "Account has Contributors Enabled:", api.me().contributors_enabled
-print "Account Creation Date:", api.me().created_at
-print "Account Uses Default Profile:", api.me().default_profile
-print "Account Uses Default Profile Image:", api.me().default_profile_image
-print "Account Personal Description:", api.me().description
-print "Account Entities:", api.me().entities
-print "Account # of Twitts favoured:", api.me().favourites_count
-print "Account # of Followers:", api.me().followers_count
-print "Account is following:", api.me().following
-print "Account # of Friends:", api.me().friends_count
-print "Account is Geo-enabled:", api.me().geo_enabled
-print "Account has extended profile:", api.me().has_extended_profile
-print "Account Raw Id:", api.me().id
-print "Account Raw Id (Legacy):", api.me().id_str
-print "Account Set Language:", api.me().lang
-print "Account Listed Count:", api.me().listed_count
-print "Account User Location:", api.me().location
-print "Account User Name:", api.me().name
-print "Account Needs Phone Verification:", api.me().needs_phone_verification
-print "Account has notifications enabled:", api.me().notifications
-print "Account Profile Background Color:", api.me().profile_background_color
-print "Account Profile Background Image URL:", api.me().profile_background_image_url
-print "Account Profile Background Image URL (https):", api.me().profile_background_image_url_https
-print "Account Profile Uses Background Tile:", api.me().profile_background_tile
-print "Account Profile Banner Image URL:", api.me().profile_banner_url
-print "Account Profile Image URL:", api.me().profile_image_url
-print "Account Profile Image URL (https):", api.me().profile_image_url_https
-print "Account Profile Link Color:", api.me().profile_link_color
-print "Account Profile Location:", api.me().profile_location
-print "Account Profile Sidebar Border Color:", api.me().profile_sidebar_border_color
-print "Account Profile Sidebar Fill Color:", api.me().profile_sidebar_fill_color
-print "Account Profile Text Color:", api.me().profile_text_color
-print "Account Profile Used Background Image:", api.me().profile_use_background_image
-print "Account is Protected:", api.me().protected
-print "Account Screen Name:", api.me().screen_name
-print "Account # of Published Twitts:", api.me().statuses_count
-print "Account is suspended:", api.me().suspended
-print "Account Time Zone:", api.me().time_zone
-print "Account uses Translator Type:", api.me().translator_type
-print "Account Profile has a URL:", api.me().url
-print "Account UTC Offset (in Seconds):", api.me().utc_offset
-print "Account is verified:", api.me().verified
+def aboutme():
+    print "      User Name:", api.me().name
+    print "Screen Name (@):", api.me().screen_name
+    print "    Description:", api.me().description
+    print "  User Location:", api.me().location
+    print "      Followers:", api.me().followers_count
+    print "      Following:", api.me().friends_count
+    print "    # of Twitts:", api.me().statuses_count
+    print "  Creation Date:", api.me().created_at
+    print "      Time Zone:", api.me().time_zone
+    print "       Language:", api.me().lang
+    print "   Listed Count:", api.me().listed_count
+    print "Verified Phone?:", api.me().needs_phone_verification
+
+def aboutmeextended():
+    print "          Contributors Enabled:", api.me().contributors_enabled
+    print "          Uses Default Profile:", api.me().default_profile
+    print "    Uses Default Profile Image:", api.me().default_profile_image
+    print "                      Entities:", api.me().entities
+    print "                     Following:", api.me().following
+    print "          # of Twitts favoured:", api.me().favourites_count
+    print "                is Geo-enabled:", api.me().geo_enabled
+    print "          has extended profile:", api.me().has_extended_profile
+    print "                        Raw Id:", api.me().id
+    print "               Raw Id (Legacy):", api.me().id_str
+    print "         Notifications enabled:", api.me().notifications
+    print "      Profile Background Color:", api.me().profile_background_color
+    print "        Profile Background URL:", api.me().profile_background_image_url
+    print "Profile Background URL (https):", api.me().profile_background_image_url_https
+    print "  Profile Uses Background Tile:", api.me().profile_background_tile
+    print "      Profile Banner Image URL:", api.me().profile_banner_url
+    print "             Profile Image URL:", api.me().profile_image_url
+    print "     Profile Image URL (https):", api.me().profile_image_url_https
+    print "            Profile Link Color:", api.me().profile_link_color
+    print "              Profile Location:", api.me().profile_location
+    print "  Profile Sidebar Border Color:", api.me().profile_sidebar_border_color
+    print "    Profile Sidebar Fill Color:", api.me().profile_sidebar_fill_color
+    print "            Profile Text Color:", api.me().profile_text_color
+    print " Profile Used Background Image:", api.me().profile_use_background_image
+    print "                     Protected:", api.me().protected
+    print "                    Suspended?:", api.me().suspended
+    print "          uses Translator Type:", api.me().translator_type
+    print "             Profile has a URL:", api.me().url
+    print "       UTC Offset (in Seconds):", api.me().utc_offset
+    print "                   is verified:", api.me().verified
+
+# Long format of latest status
+# print api.me().status
+
+username = "vegamontesino"
+def currentstatus():
+    print "Twitt Content:", api.me().status.text
+    print "    Truncated:", api.me().status.truncated
+    print "   Created at:", api.me().status.created_at
+    print "  In reply to:", api.me().status.in_reply_to_screen_name
+    print "       Source:", api.me().status.source
+    print "         Favs:", api.me().status.favorite_count
+    print "     Retweets:", api.me().status.retweet_count
+    print "        Place:", api.me().status.place
+
+def currentstatusextended():
+    print ""
+    print "          Contributor:", api.me().status.contributors
+    print "         Quote Status:", api.me().status.is_quote_status
+    print "In reply to status Id:", api.me().status.in_reply_to_status_id
+    print "            Status Id:", api.me().status.id
+    print "                  API:", api.me().status._api
+    #print "         status JSON:", api.me().status._json
+    print "          Coordinates:", api.me().status.coordinates
+    #print "     Status entities:", api.me().status.entities
+    print "            Status Id:", api.me().status.id_str
+    print "  In reply to user Id:", api.me().status.in_reply_to_user_id
+    print "         Twitt is Fav:", api.me().status.favorited
+    print "           Source URL:", api.me().status.source_url
+    print "      Geolocalization:", api.me().status.geo
+    print "  In reply to User Id:", api.me().status.in_reply_to_user_id_str
+    print "       Twitt Language:", api.me().status.lang
+    print "In reply to Status Id:", api.me().status.in_reply_to_status_id_str
+    print "            Retweeted:", api.me().status.retweeted
+
+aboutme()
+print "-------------------------------------"
+currentstatus()
 
 # AttributeError: 'User' object has no attribute 'hashtags'
 #----------------------------------
-# print(api.me().coordinates)
-# print(api.me()._json)
-# print(api.me().favorite_count)
-# print(api.me().favorited)
-# print(api.me().geo)
-# print(api.me().hashtags)
-# print(api.me().in_reply_to_screen_name)
-# print(api.me().in_reply_to_status_id)
-# print(api.me().in_reply_to_status_id_str)
-# print(api.me().in_reply_to_user_id)
-# print(api.me().in_reply_to_user_id_str)
-# print(api.me().indices)
-# print(api.me().is_quote_status)
-# print(api.me().is_translation_enabled)
-# print(api.me().is_translator)
-# print(api.me().place)
-#print(api.me().retweet_count)
-#print(api.me().retweeted)
-# print(api.me().source)
-# print(api.me().source_url)
-# print(api.me().text)
-# print(api.me().truncated)
-# print(api.me().urls)
-# print(api.me().user_mentions)
-# print(api.me().User(follow_request_sent))
+# print api.me().coordinates)
+# print api.me()._json)
+# print api.me().favorite_count)
+# print api.me().favorited)
+# print api.me().geo)
+# print api.me().hashtags)
+# print api.me().in_reply_to_screen_name)
+# print api.me().in_reply_to_status_id)
+# print api.me().in_reply_to_status_id_str)
+# print api.me().in_reply_to_user_id)
+# print api.me().in_reply_to_user_id_str)
+# print api.me().indices)
+# print api.me().is_quote_status)
+# print api.me().is_translation_enabled)
+# print api.me().is_translator)
+# print api.me().place)
+#print api.me().retweet_count)
+#print api.me().retweeted)
+# print api.me().source)
+# print api.me().source_url)
+# print api.me().text)
+# print api.me().truncated)
+# print api.me().urls)
+# print api.me().user_mentions)
+# print api.me().User(follow_request_sent))
 #----------------------------------
-# print(api.me()._api)
+# print api.me()._api)
