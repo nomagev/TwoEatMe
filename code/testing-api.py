6c52077b7fd49b8faa8fe193cbe721e4a0667f1f
Nomagev <vegamontesino@msn.com>
2017-03-18 14:08:25 +0100

    Preparation for API interaction Code bit

--- code/testing-api.py
+++ code/testing-api.py
@@ -1,11 +1,34 @@
+import os
+import sys
+import pickle
 import tweepy
 
-# Credentials here: # Check credentials-DO-NOT-COMMIT.txt
-consumer_key = 
-consumer_secret = 
-access_token = 
-access_token_secret = 
-# Credentials here: # Check credentials-DO-NOT-COMMIT.txt
+# pylint: disable=C0103
+# -*- coding: utf-8 -*-
+
+clear()
+
+filename = os.path.basename(__file__)
+fileexists = os.path.exists("keys-DO-NOT-COMMIT.txt")
+
+if fileexists is False:
+    consumer_key = raw_input("1 - Enter your Consumer Key: ")
+    consumer_secret = raw_input("2 - Enter your Consumer Secret Key: ")
+    access_token = raw_input("3 - Enter your Access Token: ")
+    access_token_secret = raw_input("4 - Enter your Access Token Secret Key: ")
+    keylist = [consumer_key, consumer_secret, access_token, access_token_secret]
+    with open('keys-DO-NOT-COMMIT.txt', "wb") as keyloader:
+        pickle.dump(keylist, keyloader)
+
+else:
+    with open('keys-DO-NOT-COMMIT.txt', "rb") as keyloader:
+        keylist = pickle.load(keyloader)
+        consumer_key = keylist[0]
+        consumer_secret = keylist[1]
+        access_token = keylist[2]
+        access_token_secret = keylist[3]
+
+# This will be the part where we will work on the API interaction.
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
