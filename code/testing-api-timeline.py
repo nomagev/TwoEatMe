4aa04503c3ddaa8feca40085a43f65c297cae202
nomaveg <vegamontesino@msn.com>
2017-03-21 21:28:15 +0100

    Working on Timeline

---
+++ code/testing-api-timeline.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import pickle
+import tweepy
+
+# pylint: disable=C0103
+# -*- coding: utf-8 -*-
+
+if sys.platform == "linux" or sys.platform == "linux2":
+    clear = lambda: os.system('clear')
+    pause = lambda: os.system('read -p "$*"')
+elif sys.platform == "darwin":
+    clear = lambda: os.system('clear')
+    pause = lambda: os.system('read -p "$*"')
+elif sys.platform == "win32":
+    clear = lambda: os.system('cls')
+    pause = lambda: os.system("pause")
+
+clear()
+
+filename = os.path.basename(__file__)
+fileexists = os.path.exists("keys-DO-NOT-COMMIT.txt")
+
+if fileexists is False:
+    consumer_key = raw_input("1 - Enter your Consumer Key: ")
+    consumer_secret = raw_input("2 - Enter your Consumer Secret Key: ")
+    access_token = raw_input("3 - Enter your Access Token: ")
+    access_token_secret = raw_input("4 - Enter your Access Token Secret Key: ")
+    keylist = [consumer_key, consumer_secret, access_token, access_token_secret]
+    with open('keys-DO-NOT-COMMIT.txt', "wb") as keyloader:
+        pickle.dump(keylist, keyloader)
+
+else:
+    with open('keys-DO-NOT-COMMIT.txt', "rb") as keyloader:
+        keylist = pickle.load(keyloader)
+        consumer_key = keylist[0]
+        consumer_secret = keylist[1]
+        access_token = keylist[2]
+        access_token_secret = keylist[3]
+
+# Authorization and Credentials
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Timeline
+
+twitter_home_timeline = api.home_timeline(count=1)
+twitter_user_timeline = api.user_timeline(count=1)
+
+print twitter_home_timeline
+print "-----------------"
+print twitter_user_timeline
