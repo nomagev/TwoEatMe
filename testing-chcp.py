122c70bd28452f67412b53fecc233e1329a3a77c
nomaveg <vegamontesino@msn.com>
2017-03-27 23:26:11 +0200

    testing new ways to handle chcp change on prompt

--- testing-chcp.py
+++ testing-chcp.py
@@ -2,12 +2,12 @@
 # Handling Proper CHCP setup on Shell
 # https://github.com/nomagev/TwoEatMe/issues/13
 
-import os
-
-# pylint: disable=C0103
-# -*- coding: utf-8 -*-
-
-subprocess.check_output(['cmd.com'])
-print table(['chcp.com'])
-
-#end
+import subprocess
+ 
+p = subprocess.Popen("chcp.com", stdout=subprocess.PIPE, shell=True)
+ 
+(output, err) = p.communicate()
+ 
+p_status = p.wait()
+print "Command output : ", output
+print "Command exit status/return code : ", p_status
